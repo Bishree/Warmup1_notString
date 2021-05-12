5563c643045dfb8f5d3716349c298b1881f88e63
Bishree <ahmedbishree@gmail.com@users.noreply.github.com>
2021-05-12 03:22:59 +0300

    _second_commit

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+fn notString (any_string:String) -> String {
+    //
+    if (any_string.len() < 3) || (&any_string [..3] != "not") {
+            format!("not {}",any_string)
+    }else {
+        any_string.to_string()
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut new_string= "ahmed";
+    println!("{}", notString (new_string.to_string()));
 }
